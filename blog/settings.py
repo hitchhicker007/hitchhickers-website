163eb879567ce705017041c811c8a13b59203cc8
Hitch hicker <51155358+hitchhicker007@users.noreply.github.com>
2019-12-18 19:56:10 +0530

    First commit

--- blog/settings.py
+++ blog/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
